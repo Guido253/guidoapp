0c348087438d51dc18678d07f6c32c2e18e22ad3
guidomazzarelli <guidonmazzarelli@gmail.com>
2022-06-16 18:03:39 -0300

    nuevos cambios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,21 @@
 import './Navbar.css';
-
+import foto from '../assets/img/foto.png';
 
 
 function Navbar() {
   return (
     <header>
       <nav className='navbar'>
-      <img className='logo' src='./assets/img/logo.png'></img>
+      <img className='fotologo' src= {foto}></img>
       <ul className='navLinks'>
         <li>
-        <a href='http://localhost:3000/'>Servicios</a>
+        <a href='http://localhost:3000/'>Tragos</a>
         </li>
         <li>
-          <a href='http://localhost:3000/'>Proyectos</a>
+          <a href='http://localhost:3000/'>Comidas</a>
         </li>
         <li>
-        <a href='http://localhost:3000/'>Tienda Online!</a> 
+        <a href='http://localhost:3000/'>Cervezas</a> 
         </li>
         </ul>
         <a className='boton' href='#'><button>Contacto</button></a>
